27849f8a091cf53226bc8059e5880a77261c4a7f
Utkarsh <63993825+hindustryes@users.noreply.github.com>
2020-12-28 17:36:58 -0800

    Delete Script.js

--- Script.js
+++
@@ -1,5 +0,0 @@
-// Created by ❗Close ❗Dheerajkanaujiya
-
-
-
-alert ("💞🌴🧚‍♂️💞🌴2💞🧚‍♂️2🌴\nPlease seen in the best network and\nPlease touch the car\n🤔I Think🤔\n🙏Human desires are infinite\nSo suppose\nWhich state would you like to roam with this express\nGive me a comment\nPlease\nPlease..........🙏")
